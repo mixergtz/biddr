1cc42199a696cef7556ad199d5b3c55da4e480a1
Mixer Gutierrez <mixergutierrez@gmail.com>
2017-01-26 23:35:27 -0500

    Add css love

--- app/assets/javascripts/channels/bids.js
+++ app/assets/javascripts/channels/bids.js
@@ -2,10 +2,13 @@ App.cable.subscriptions.create("BidsChannel", {
   received: function(data) {
     // Main table
     $(".bids-table tbody")
-    .prepend("<tr><td>" + data.product_name + "</td><td>" + data.value + "</td></tr>")
+    .prepend("<tr class='warning'><td>" + data.product_name +
+             "</td><td>" + data.value + "<span class='label label-info pull-right'> \
+             New</span></td></tr>")
 
     // Product specific tables
     $(".bids-table-" + data.product_id + " tbody")
-    .prepend("<tr><td>" + data.value + "</td></tr>")
+    .prepend("<tr class='warning'><td>" + data.value + " \
+             <span class='label label-info pull-right'>New</span></td></tr>")
   }
 });
